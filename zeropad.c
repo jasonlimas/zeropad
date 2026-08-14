26b8bc17aa697bb9f5e129e63ee890bcab6ddabb
Jason Limas <jasonandrea14@gmail.com>
2026-08-14 16:36:03 +0700

    add raw terminal mode, keypress loop, screen clearing
    - Enter raw mode via termios (disable ICANON, ECHO)
    - Restore terminal cleanly on exit
    - Clear screen and reposition cursor via ANSI escape sequences each
    keypress
    - Detect terminal size via ioctl/TIOCGWINSZ

--- zeropad.c
+++ zeropad.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -8,7 +11,7 @@ int main(int argc, char *argv[]) {
 
     FILE *fp = fopen(argv[1], "r");
 
-    if(fp == NULL) {
+    if (fp == NULL) {
         printf("%s\n", "Error reading file");
         return 1;
     }
@@ -19,6 +22,36 @@ int main(int argc, char *argv[]) {
     }
 
     fclose(fp);
+
+    struct termios orig_termios;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    struct winsize ws;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+
+    // Terminal size detection
+    printf("terminal size: %d, %d\n", (int)ws.ws_row, (int)ws.ws_col);
+
+    int c;
+    while (1) {
+        c = getchar();
+
+        // Clear the screen and move cursor to top left before printing something else
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+
+        printf("key: %c (%d)\r\n", c, c);
+
+        // Quit
+        if (c == 'q') break;
+    }
+    
+    // Cleanly reset the terminal
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); 
 
     return 0;
 }
